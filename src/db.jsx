16ed6d28d0ad9d6bb00a0ef98b7ff3d82ad38121
Ehlakya <ehlakyas@gmail.com>
2025-11-01 12:41:02 +0530

    fix

---
+++ src/db.jsx
@@ -0,0 +1,12 @@
+import user1 from '../assets/user1.jpeg';
+import img1 from '../assets/user_image1.jpg';
+
+const post = {
+  id: 1,
+  user: {
+    username: 'john_doe',
+    profile: user1,
+  },
+  image: img1,
+  caption: 'Enjoying the beach vibes! 🌊☀️ #vacation',
+};
