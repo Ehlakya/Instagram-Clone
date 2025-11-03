16666dee5ad80012aab0c659fc110a36b659fe5b
Ehlakya <ehlakyas@gmail.com>
2025-11-03 10:02:12 +0530

    fix

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,50 +1,90 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import { post } from '../db/db';
 
-function Posts() {
+// function Posts() {
 
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-
-    fetch('https://instagram-clone-theta-seven.vercel.app/posts')
-      .then((data) => data.json())
-      .then((data) => setPosts(data))
-      .catch((err) => console.log(err))
-
-  }, []);
+//   const [posts, setPosts] = useState([]);
 
 
-  return (
-    <div className='d-flex justify-content-center'>
-      {posts.length > 0 ? (
-        <div>
-          {posts.map((post)=>(
-            <div className='my-3' key={post.id}>
-              <div className='d-flex'>
-                <img className='dp rounded-circle' src={post.user.profile} alt="profile_pic" />
-                <h5>{post.user.username}</h5>
-              </div>
-              <img className='image' src={post.image} alt="post" />
-              <div>
-                <i className="bi bi-heart"></i>
-                <i className="bi bi-chat"></i>
-                <i className="bi bi-send"></i>
-              </div>
-              <div>
-                <b>{post.likes}Likes</b>
-              </div>
-              <p>{post.caption}</p>
-            </div>
-          ))}
+  
+
+//   useEffect(() => {
+
+//     fetch('https://instagram-clone-theta-seven.vercel.app/posts')
+//       .then((data) => data.json())
+//       .then((data) => setPosts(data))
+//       .catch((err) => console.log(err))
+
+//   }, []);
+ 
+
+//   return (
+//     <div className='d-flex justify-content-center'>
+//       {posts.length > 0 ? (
+//         <div>
+//           {posts.map((post)=>(
+//             <div className='my-3' key={post.id}>
+//               <div className='d-flex'>
+//                 <img className='dp rounded-circle' src={post.user.profile} alt="profile_pic" />
+//                 <h5>{post.user.username}</h5>
+//               </div>
+//               <img className='image' src={post.image} alt="post" />
+//               <div>
+//                 <i className="bi bi-heart"></i>
+//                 <i className="bi bi-chat"></i>
+//                 <i className="bi bi-send"></i>
+//               </div>
+//               <div>
+//                 <b>{post.likes}Likes</b>
+//               </div>
+//               <p>{post.caption}</p>
+//             </div>
+//           ))}
           
-        </div>
-      ):(
-        <div>
-          Loding Posts
-        </div>
-      )}
+//         </div>
+//       ):(
+//         <div>
+//           Loding Posts
+//         </div>
+//       )}
 
+//     </div>
+//   )
+// }
+// export default Posts
+
+
+import React from "react";
+import { posts } from "./postsData";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      {posts.map((post) => (
+        <div key={post.id} className="post">
+          <div className="post-header">
+            <img src={post.user.profile} alt={post.user.username} className="profile-pic" />
+            <span>{post.user.username}</span>
+          </div>
+
+          <img src={post.image} alt="Post" className="post-image" />
+
+          <div className="post-details">
+            <p><strong>{post.user.username}</strong> {post.caption}</p>
+            <p>❤️ {post.likes} likes</p>
+
+            <div className="comments">
+              {post.comments.map((c, index) => (
+                <p key={index}>
+                  <strong>{c.username}</strong> {c.text}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
-export default Posts
+  );
+};
+
+export default Feed;
